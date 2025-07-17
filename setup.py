738d3fbdea2357ac7feecd755c29fa5f8383680d
seanhill <seamhill93@gmail.com>
2025-07-17 03:22:16 -0400

    fix imports and update version

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+
+def parse_requirements(filename):
+    with open(filename, 'r') as f:
+        return f.read().splitlines()
 
 setup(name='alphavantage',
-      version='0.1.6',
+      version='0.1.7',
       description='A python wrapper to the Alpha Vantage API for financial data',
       url='https://github.com/shill1729/alphavantage',
       author='S. Hill',
       author_email='52792611+shill1729@users.noreply.github.com',
       license='MIT',
-      packages=['alphavantage'],
-      install_requires=[
-          'numpy',
-          'pandas',
-          "requests",
-          "finnhub-python"
-      ],
+      packages=find_packages(include=["alphavantage", "alphavantage.*"]),
+      install_requires=parse_requirements("requirements.txt"),
       zip_safe=False)
-
